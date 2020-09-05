81fc52b661f6f1a18dd23e5668fec8246418f4ab
Victor Gabardini <victorgabardini@gmail.com>
2020-09-05 00:39:40 -0300

    primer commit desde django a heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mf#@-cofbv@*(-h9o1u4h@65ad14u=e16y5)hh@ix48l47m@*i'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','mf#@-cofbv@*(-h9o1u4h@65ad14u=e16y5)hh@ix48l47m@*i')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
