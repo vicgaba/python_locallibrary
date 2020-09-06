3ae6b7fcc978b30cf2677beedc225b70a47c0654
vicgaba <victorgabardini@gmail.com>
2020-09-06 12:46:05 -0300

    versiton de MDN

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,40 +1,75 @@
 from django.contrib import admin
-from .models import Author, Book, BookInstance, Genre, Language
 
 # Register your models here.
 
-#admin.site.register(Book)
-#admin.site.register(Author)
-#admin.site.register(BookInstance)
+from .models import Author, Genre, Book, BookInstance, Language
+
+"""Minimal registration of Models.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
+"""
+
 admin.site.register(Genre)
 admin.site.register(Language)
 
-class BookInline(admin.TabularInline):
+
+class BooksInline(admin.TabularInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
     model = Book
 
-#definimos la clase admin para autor
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name',  'date_of_birth', 'date_of_death')
-    inlines = [BookInline]
-#registramos la clase admin con el modelo asociado
-admin.site.register(Author, AuthorAdmin)
-class BookInstanceInline(admin.TabularInline):
+    """Administration object for Author models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields),
+       grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
+    list_display = ('last_name',
+                    'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    """Defines format of inline book instance insertion (used in BookAdmin)"""
     model = BookInstance
 
-@admin.register(Book)
+
 class BookAdmin(admin.ModelAdmin):
+    """Administration object for Book models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
     list_display = ('title', 'author', 'display_genre')
-    inlines = [BookInstanceInline]
+    inlines = [BooksInstanceInline]
+
+
+admin.site.register(Book, BookAdmin)
+
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display= ('book', 'status', 'due_back')
+    """Administration object for BookInstance models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - filters that will be displayed in sidebar (list_filter)
+     - grouping of fields into sections (fieldsets)
+    """
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
+
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
         }),
-        ('Disponibilidad', {
-            'fields': ('status', 'due_back')
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
